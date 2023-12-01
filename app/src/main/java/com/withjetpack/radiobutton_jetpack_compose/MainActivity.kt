dcedee652ca2949ac7283bc39a811c4bcb42416a
chayan chowdhury <chayanchowdhury43@yahoo.com>
2023-12-01 17:09:39 +0530

    Implemeted Radio option and added onclick listener

--- app/src/main/java/com/withjetpack/radiobutton_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/withjetpack/radiobutton_jetpack_compose/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.withjetpack.radiobutton_jetpack_compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.withjetpack.radiobutton_jetpack_compose.ui.theme.RadioButtonJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +28,51 @@ class MainActivity : ComponentActivity() {
             RadioButtonJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SampleRadioButton()
                 }
             }
         }
     }
+
+
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SampleRadioButton() {
+    val radioOptions = listOf("Apple", "Google", "Facebook")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[2]) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RadioButtonJetpackComposeTheme {
-        Greeting("Android")
+        Column() {
+            radioOptions.forEach { text ->
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .selectable(selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) })
+                ) {
+                    val context = LocalContext.current
+
+                    RadioButton(selected = (text == selectedOption),
+                        onClick = {
+                            onOptionSelected(text)
+                            Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+                        })
+                    Text(
+                        text = text, modifier = Modifier.padding(start = 3.dp).align(Alignment.CenterVertically)
+                    )
+                }
+            }
+
+        }
+
+
     }
+
 }
